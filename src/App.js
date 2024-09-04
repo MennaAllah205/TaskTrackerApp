ef6f1d22ffaf12fdc8b92ad3738fe05a753d9f55
sobhymenna40@gmail.com <yourfavmoon>
2024-09-04 21:16:54 +0300

    React

--- src/App.js
+++ src/App.js
@@ -4,44 +4,49 @@ import ToDo from "./Components/ToDo";
 import "./Css/App.css";
 
 const App = () => {
-  const handleClearAll = () => {
-    setTodos([]);
-  };
   const [todos, setTodos] = useState([]);
+  const [todoShow, setTodoShow] = useState("all");
+
   const addTodo = (todo) => {
     setTodos([todo, ...todos]);
   };
+
   const handleDelete = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
-  const [todoShow, setTodoShow] = useState("all");
+
   const updateTodoToShow = (s) => {
     setTodoShow(s);
   };
-  if (todoShow === "active") {
-    todos = todos.filter((todo) => !todo.complete);
-  } else if (todoShow === "complete") {
-    todos = todos.filter((todo) => todo.complete);
-  }
+
   const toggleComplete = (id) => {
     setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            complete: !todo.complete,
-          };
-        } else {
-          return todo;
-        }
-      })
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, complete: !todo.complete } : todo
+      )
     );
   };
+
+  const filteredTodos = () => {
+    switch (todoShow) {
+      case "active":
+        return todos.filter((todo) => !todo.complete);
+      case "complete":
+        return todos.filter((todo) => todo.complete);
+      default:
+        return todos;
+    }
+  };
+
+  const handleClearAll = () => {
+    setTodos([]);
+  };
+
   return (
     <>
       <div>
         <ToDoForm onSubmit={addTodo} />
-        {todos.map((todo) => (
+        {filteredTodos().map((todo) => (
           <ToDo
             key={todo.id}
             todo={todo}
